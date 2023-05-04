4ccb2a3224a1fa0c580d47c08889ba52c2fd7d4b
Amr-Safwat <amrsafwat02@gmail.com>
2023-05-04 15:57:23 +0200

    added some modifires

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 echo "<h1>Web Exploit Scan 2023</h1>";
-$ip = " ip : ".$_SERVER['REMOTE_ADDR'];
-$user = "\n System and browser : ".$_SERVER['HTTP_USER_AGENT'];
-$coun = "\n Country : ".$_SERVER['HTTP_ACCEPT_LANGUAGE'];
+$ip = " ip: ".$_SERVER['REMOTE_ADDR'];
+$user = "\n System and browser: ".$_SERVER['HTTP_USER_AGENT'];
+$coun = "\n Country: ".$_SERVER['HTTP_ACCEPT_LANGUAGE'];
 
 $filoo = fopen('security.txt','a');
 fwrite($filoo, "\nuser:\n");
